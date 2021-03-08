ff07108bbd76a9c5b63a737e12062a5b0de56120
Ivan Lam <ivnater@gmail.com>
2021-03-08 11:34:15 -0500

    Added code and readme

--- fretboard-learner.py
+++ fretboard-learner.py
@@ -1,5 +1,38 @@
+import random
+import time
+
+notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+random.seed()
+
 def main():
-    print("Hello World!")
+    print("###################################")
+    print("## Welcome to Fretboard Trainer! ##")
+    print("###################################")
+    print("Press c if you got it correct and w for wrong")
+    print("Type q to quit")
+    correct = 0
+    correctTime = 0.
+    wrongTime = 0.
+    wrong = 0
+    response = ""
+    print("----------------------------------")
+    while response != "q":
+        randomNote = notes[random.randint(0, len(notes) - 1)]
+        print("Play: " + randomNote)
+        start = time.time()
+        response = input()
+        end = time.time()
+        if response == "c":
+            correctTime = end - start
+            correct = correct + 1
+        else:
+            wrongTime = end - start
+            wrong = wrong + 1
+        print("----------------------------------")
+    print("Final result: ")
+    print("Correct: " + str(correct) + ", avg response time: " + str(round(correctTime / correct, 5)) + " s")
+    print("Wrong: " + str(wrong) + ", avg response time: " + str(round(wrongTime / wrong, 5)) + " s")
+
 
 if __name__ == "__main__":
     main()
---
+++ readme.md
@@ -0,0 +1,17 @@
+### Fretboard-Trainer ###
+Just a simple python3 commandline script that randomly picks a musical note, helpful for learning a guitar fretboard. 
+To run it, just do (provided you have python3 installed):
+> python fretboard-learner.py
+
+# Typical Usage #
+The typical usage is:
+    - Run the script
+    - Play the note that is displayed
+    - If able to find that note, enter `c`, otherwise enter `w`.
+    - Repeat until satsified, then enter `q` to finish. 
+    - Some final stats should pop up at the end.
+
+Although this is the typical usage, this program is pretty general and can be really used for any sort of excercise that you can think of. 
+
+# Future #
+At the moment this program is *extremely* rudementary, and was mostly born out of the fact that I didn't feel like making paper cue cards to practice notes on my guitar fretboard. 
