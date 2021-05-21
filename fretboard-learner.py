351dbd11605dcf026a31094ea9eec3a6a2eb8aba
Ivan Lam <ivnater@gmail.com>
2021-05-21 10:40:02 -0400

    Added option to not track wrong notes

--- fretboard-learner.py
+++ fretboard-learner.py
@@ -8,26 +8,36 @@ def main():
     print("###################################")
     print("## Welcome to Fretboard Trainer! ##")
     print("###################################")
-    print("Press c if you got it correct and w for wrong")
+    print("Press enter to go to next note, type \"w\" if you want to track \"wrongs\"")
     print("Type q to quit")
     correct = 0
     correctTime = 0.
     wrongTime = 0.
     wrong = 0
     response = ""
+    includeStrings = input("Do you want to include strings? (Y/n): ")
+    rounds = input("Enter amount of rounds (leave blank for endless): ")
+    if rounds != '':
+        rounds = int(rounds)
+    else:
+        rounds = -1
     print("----------------------------------")
-    while response != "q":
+    while response != "q" and ((rounds == -1) or (rounds > (correct + wrong))):
         randomNote = notes[random.randint(0, len(notes) - 1)]
-        print("Play: " + randomNote)
+        randomString = random.randint(1, 6)
+        if includeStrings == "n":
+            print("Play: " + randomNote)
+        else:
+            print("Play: " + randomNote + " on string: " + str(randomString))
         start = time.time()
         response = input()
         end = time.time()
-        if response == "c":
-            correctTime = correctTime + (end - start)
-            correct = correct + 1
-        elif response == "w":
+        if response == "w":
             wrongTime = wrongTime + (end - start)
             wrong = wrong + 1
+        else:
+            correctTime = correctTime + (end - start)
+            correct = correct + 1
         print("----------------------------------")
     print("Final result: ")
     print("Correct: " + str(correct) + ", avg response time: " + str(round(correctTime / correct, 5) if correct else 0) + " s")
